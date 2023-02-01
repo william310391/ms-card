325a64f4d38207866658251a8173ea6d68d92147
William <william310391@gmail.com>
2023-01-31 22:46:56 -0500

    Initial commit

---
+++ src/main/java/com/crud/card/model/Card.java
@@ -0,0 +1,12 @@
+package com.crud.card.model;
+import  lombok.Data;
+
+@Data
+public class Card {
+    int id_card;
+    String name;
+    String number;
+    String type;
+    int cvv;
+    int status;
+}
---
+++ src/main/java/com/crud/card/service/ICardService.java
@@ -0,0 +1,10 @@
+package com.crud.card.service;
+import com.crud.card.model.Card;
+import java.util.List;
+
+public interface ICardService {
+    public List<Card> findAll();
+    public int save(Card card);
+    public int update(Card card);
+    public int deleteById(int id);
+}
